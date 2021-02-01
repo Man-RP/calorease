82b2a3797c7f2ee445c8bf6ce64ac702d526b2e8
Daniel Manor <danielmanor.lab@gmail.com>
2021-02-01 16:50:07 +0200

    feat: show button stories in storybook interface

--- src/components/stories/Button.stories.tsx
+++ src/components/stories/Button.stories.tsx
@@ -3,12 +3,21 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import Button from '../atoms/Button';
 import CenterView from '../../../storybook/stories/CenterView';
+import {boolean, text, withKnobs} from '@storybook/addon-knobs';
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .addDecorator((getStory: Function) => <CenterView>{getStory()}</CenterView>)
-  .add('wide', () => (
-    <Button onPress={action('clicked-wide')} title={'הרשמה'} wide />
+  .add('default', () => (
+    <Button
+      onPress={action('clicked-wide')}
+      title={text('title', 'כותרת')}
+      wide={boolean('wide', true)}
+    />
   ))
   .add('narrow', () => (
     <Button onPress={action('clicked-narrow')} title={'הוסף ארוחה'} />
+  ))
+  .add('wide', () => (
+    <Button wide onPress={action('clicked-narrow')} title={'הרשמה'} />
   ));
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
 import {Dimensions} from 'react-native';
+import {I18nManager} from 'react-native';
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
+const isRTL = I18nManager.isRTL;
 
 import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
 
-export {screenWidth, screenHeight, RFPercentage, RFValue};
+export {screenWidth, screenHeight, RFPercentage, RFValue, isRTL};
